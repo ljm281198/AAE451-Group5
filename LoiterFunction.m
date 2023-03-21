bef8547231076b418d8df997706e8aeab6f15835
cblarsen21 <larsec16@gmail.com>
2023-03-21 16:02:26 -0400

    loiter_test
    i added a comment

--- LoiterFunction.m
+++ LoiterFunction.m
@@ -11,7 +11,7 @@
 function output = LoiterFunction(inputs,Wi)
 
 %% Inputs for loiter fuel computations
-time   = inputs.MissionInputs.loiter_time;           % Loiter time [hours]
+time  = inputs.MissionInputs.loiter_time;           % Loiter time [hours]
 eta_p  = inputs.PropulsionInputs.eta_p;              % propeller efficiency
 c_bhp  = inputs.PropulsionInputs.c_bhp;              % Propeller specific fuel consumption [lb/hr]
 %%
@@ -32,7 +32,9 @@ c_bhp  = inputs.PropulsionInputs.c_bhp;              % Propeller specific fuel c
   fl          = exp(-time*(inputs.Aero.V*0.7)*(c_bhp+0.05)/(325.9*LDrat*(eta_p-0.05)));              % loiter fuel weight fraction
   Wf          = Wi*fl;                           % final aircraft weight after loiter segment
   output.f_lt = Wf/Wi;                           % loiter fuel-weight ratio (for entire segment)
-  output.fuel = Wi-Wf;                           % total loiter fuel [lbs]
+  output.fuel = Wi-Wf;   % total loiter fuel [lbs]
+  %this is a comment
+  
 end
 
 
